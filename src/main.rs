2612eacec13f919d206f5c7a8ea88449fa8f4fe8
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-03-21 12:20:57 +0100

    Add imperial march song and clean cycle

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,20 @@ extern crate panic_halt;
 
 use arduino_uno::delay_ms;
 use arduino_uno::prelude::*;
-use rumba::Rumba;
+use embedded_hal::serial::{Read, Write};
+use rumba::{prelude::*, Note, NoteName, NoteOctave, Rumba};
 use timer::{AvrTimer1, U32Ext};
+
+fn clean_a_bit<T>(rumba: &mut Rumba<T, rumba::mode::Passive>) -> Result<(), <T as Write<u8>>::Error>
+where
+    T: Write<u8> + Read<u8>,
+{
+    rumba.max_clean()?;
+    delay_ms(5000);
+    rumba.max_clean()?;
+    delay_ms(1000);
+    Ok(())
+}
 
 fn app() {
     let dp = arduino_uno::Peripherals::take().unwrap();
@@ -26,7 +38,7 @@ fn app() {
     timer.start(115200.hz());
 
     let mut rumba_tx = pins.d2.into_output(&pins.ddr);
-    rumba_tx.set_high();
+    rumba_tx.set_high().unwrap();
     let rumba_serial = bitbang_hal::serial::Serial::new(rumba_tx, pins.d3, timer);
     let rumba = Rumba::new(rumba_serial);
 
@@ -34,13 +46,120 @@ fn app() {
     let mut rumba = rumba.into_passive();
     delay_ms(1000);
     ufmt::uwriteln!(&mut serial, "Sending first song").unwrap();
-    rumba.send_song(rumba::SongSlot::First).unwrap();
+    rumba
+        .send_song(
+            rumba::SongSlot::First,
+            &[
+                Note {
+                    name: NoteName::A,
+                    octave: NoteOctave::Small,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::A,
+                    octave: NoteOctave::Small,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::A,
+                    octave: NoteOctave::Small,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::F,
+                    octave: NoteOctave::Small,
+                    duration: 400.ms(),
+                },
+                Note {
+                    name: NoteName::C,
+                    octave: NoteOctave::OneLined,
+                    duration: 200.ms(),
+                },
+                Note {
+                    name: NoteName::A,
+                    octave: NoteOctave::Small,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::F,
+                    octave: NoteOctave::Small,
+                    duration: 400.ms(),
+                },
+                Note {
+                    name: NoteName::C,
+                    octave: NoteOctave::OneLined,
+                    duration: 200.ms(),
+                },
+                Note {
+                    name: NoteName::A,
+                    octave: NoteOctave::Small,
+                    duration: 1200.ms(),
+                },
+            ],
+        )
+        .unwrap();
+
+    rumba
+        .send_song(
+            rumba::SongSlot::Second,
+            &[
+                Note {
+                    name: NoteName::E,
+                    octave: NoteOctave::OneLined,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::E,
+                    octave: NoteOctave::OneLined,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::E,
+                    octave: NoteOctave::OneLined,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::F,
+                    octave: NoteOctave::OneLined,
+                    duration: 400.ms(),
+                },
+                Note {
+                    name: NoteName::C,
+                    octave: NoteOctave::OneLined,
+                    duration: 200.ms(),
+                },
+                Note {
+                    name: NoteName::GSharp,
+                    octave: NoteOctave::Small,
+                    duration: 600.ms(),
+                },
+                Note {
+                    name: NoteName::F,
+                    octave: NoteOctave::Small,
+                    duration: 400.ms(),
+                },
+                Note {
+                    name: NoteName::C,
+                    octave: NoteOctave::OneLined,
+                    duration: 200.ms(),
+                },
+                Note {
+                    name: NoteName::A,
+                    octave: NoteOctave::Small,
+                    duration: 1200.ms(),
+                },
+            ],
+        )
+        .unwrap();
     delay_ms(1000);
     let mut rumba = rumba.into_safe();
     delay_ms(1000);
     ufmt::uwriteln!(&mut serial, "Playing first song").unwrap();
     rumba.play_song(rumba::SongSlot::First).unwrap();
-    delay_ms(2500);
+    delay_ms(4800); // Sufficient delay for the song
+    rumba.play_song(rumba::SongSlot::Second).unwrap();
+    delay_ms(4800); // Sufficient delay for the song
+    clean_a_bit(&mut rumba.into_passive()).unwrap();
     ufmt::uwriteln!(&mut serial, "Done!").unwrap();
 }
 
